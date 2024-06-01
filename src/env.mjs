890174564d4734a3b057f071a9f21e6212802be8
Daniel Mathisen Breivik <danielmbreivik@hotmail.com>
2024-06-01 19:57:55 +0200

    Update env.mjs

--- src/env.mjs
+++ src/env.mjs
@@ -10,6 +10,7 @@ export const env = createEnv({
     DATABASE_URL: z.string(),
     POSTGRES_PRISMA_URL: z.string(),
     POSTGRES_URL_NON_POOLING: z.string(),
+    WEB_URL:z.string()
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -52,6 +53,7 @@ export const env = createEnv({
     // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     STEAM_API_KEY: process.env.STEAM_API_KEY,
+    WEB_URL: 'https://www.danielmb.party/'
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
